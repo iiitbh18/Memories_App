dc0aaea09df2960b0b532f5f8a210e101e0d01bc
Bhaskar <67088894+iiitbh18@users.noreply.github.com>
2022-09-11 18:02:32 +0530

    5th commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit:'30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
